3c3b02e5b165f5f8f1fcf0b6cdb6ef3a3cc220d5
karthik <chkarthik853@gmail.com>
2025-06-26 08:12:36 +0530

    commit

--- app/src/main/java/com/carcar/backhome/MainActivity.java
+++ app/src/main/java/com/carcar/backhome/MainActivity.java
@@ -1,5 +1,7 @@
 package com.carcar.backhome;
 
+
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
@@ -10,12 +12,19 @@ import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.Firebase;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    List<item> list=new ArrayList<>();
+    RecyclerView r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +32,20 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         Window window=getWindow();
-        window.setStatusBarColor(ContextCompat.getColor(this,R.color.white));
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.pink));
         DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
         mDatabase.child("Message").setValue("HI").addOnSuccessListener(aVoid ->{
             Log.d("Firebase", "Data written successfully");
         }).addOnFailureListener(e -> {
             Log.d("Firebase","Data written unsuccessfully!");
         });
+        list.add(new item("outline_1k_24","karthik"));
+        r=findViewById(R.id.r);
+
+        r.setLayoutManager(new LinearLayoutManager(this));
+        itemAdapter adapter=new itemAdapter(list);
+        r.setAdapter(adapter);
+        Log.d("list", String.valueOf(list.size()));
+
     }
 }
